633aee7e34f2ef17e508f2f55827937c08d1827b
azmain-siam <siamhossain2700@gmail.com>
2024-03-22 11:48:20 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import Blogs from "./Pages/Blogs";
+import Bookmarks from "./Pages/Bookmarks";
+import Home from "./Pages/Home";
+
+function App() {
+  return (
+    <>
+      <Home />
+      <Blogs />
+      <Bookmarks />
+    </>
+  );
+}
+
+export default App;
---
+++ src/Pages/Home.jsx
@@ -0,0 +1,14 @@
+import Navbar from "../Components/Navbar";
+
+const Home = () => {
+  return (
+    <div>
+      <div className="h-[68px]">
+        <Navbar />
+      </div>
+      <h1>Home</h1>
+    </div>
+  );
+};
+
+export default Home;
